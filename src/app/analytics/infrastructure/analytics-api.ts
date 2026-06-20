3f220cabfbc284adaf649746eda2c01d892e1c42
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-20 05:20:07 -0500

    refactor(analytics): connect AnalyticsApi to real backend

--- src/app/analytics/infrastructure/analytics-api.ts
+++ src/app/analytics/infrastructure/analytics-api.ts
@@ -2,23 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import {
-  EquipmentUsageStatResource,
-  EquipmentResource,
-} from './analytics-response';
 
 @Injectable({ providedIn: 'root' })
 export class AnalyticsApi {
-  private readonly statsUrl = `${environment.apiProvider}${environment.equipmentUsageStatsEndpoint}`;
-  private readonly equipUrl = `${environment.apiProvider}${environment.equipmentEndpoints}`;
+  private readonly base = environment.apiBase;
 
   constructor(private readonly http: HttpClient) {}
 
-  getUsageStats(): Observable<EquipmentUsageStatResource[]> {
-    return this.http.get<EquipmentUsageStatResource[]>(this.statsUrl);
+  requestActivityAnalysis(body: {
+    minutesActive: number; minutesInactive: number;
+    downtimeReason: string; percentageChange: number;
+  }): Observable<any> {
+    return this.http.post(`${this.base}/activity-reports`, body);
   }
 
-  getEquipments(): Observable<EquipmentResource[]> {
-    return this.http.get<EquipmentResource[]>(this.equipUrl);
+  requestMaintenanceQuote(body: {
+    amount: number; currency: string; costTypeValue: string;
+    partName: string; quantity: number; unitPrice: number;
+  }): Observable<any> {
+    return this.http.post(`${this.base}/maintenance-quotes`, body);
+  }
+
+  requestROIProjection(body: { expectedRoiPercentage: number }): Observable<any> {
+    return this.http.post(`${this.base}/roi-projections`, body);
   }
 }
