5e4c482ff8dd717dadce46799cbd0241694d2ec3
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-20 05:17:49 -0500

    chore(env): simplify environment to single apiBase URL

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,4 @@
 export const environment = {
   production: false,
-  apiProvider: 'http://localhost:3000/',
-  equipmentEndpoints: 'equipments',
-  iotEndpoint: 'iot_devices',
-  maintenanceTicketsEndpoint: 'maintenance_tickets',
-  maintenanceSchedulesEndpoint: 'maintenance_schedules',
-  equipmentUsageStatsEndpoint: 'equipment_usage_stats',
+  apiBase: 'http://localhost:8080/api/v1',
 };
