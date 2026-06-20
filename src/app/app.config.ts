7f656032c67262ea8689e3572ad615e9f97cee3a
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-20 05:18:36 -0500

    feat(auth): register JWT interceptor in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { jwtInterceptor } from './auth/infrastructure/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([jwtInterceptor])),
     provideTranslateService({
       defaultLanguage: 'en',
       useDefaultLang: true,
